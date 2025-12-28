5272f43a596bf73c2511ee632b1ccc15bd4f4bf2
Aritra Debnath <152952092+AritraDeb05@users.noreply.github.com>
2025-12-28 19:34:58 +0530

    okay!!

---
+++ models/Category.js
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize';
+
+export default (sequelize) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  }, {
+    tableName: 'categories',
+    timestamps: false,
+  });
+
+  return Category;
+};
